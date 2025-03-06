f2aea915df44cead50546cc713a788febc8303f1
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-03-05 19:19:54 -0600

    update operation_logs table schema: increase service length and user length

--- database_logger/migrations/2025-03-05-234745_operation_logs/up.sql
+++ database_logger/migrations/2025-03-05-234745_operation_logs/up.sql
@@ -2,10 +2,10 @@ CREATE TYPE operation_enum AS ENUM ('create', 'update', 'delete');
 
 CREATE TABLE operation_logs (
     id SERIAL PRIMARY KEY,
-    [service] VARCHAR(26) NOT NULL, 
-    [operation] operation_enum NOT NULL DEFAULT 'create',
+    "service" VARCHAR(36) NOT NULL, 
+    "operation" operation_enum NOT NULL DEFAULT 'create',
     element_id UUID NOT NULL,
     ip INET NOT NULL,
-    user VARCHAR(64) NOT NULL,
-    [date] TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    "user" VARCHAR(128) NOT NULL,
+    "date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
