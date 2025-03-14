ccdf263fff05e0a9c2df4fa8480b0884e53911a7
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-03-13 18:32:12 -0600

    Update migration to use INET postgres' datatype

--- database_logger/migrations/2025-03-05-234745_operation_logs/up.sql
+++ database_logger/migrations/2025-03-05-234745_operation_logs/up.sql
@@ -4,7 +4,7 @@ CREATE TABLE operation_logs (
     "operation" INTEGER REFERENCES operations (id) NOT NULL,
     affected_table VARCHAR(36) NOT NULL,
     element_id UUID NOT NULL,
-    ip VARCHAR(20) NOT NULL,
+    ip INET NOT NULL,
     "user" VARCHAR(128) NOT NULL,
     "date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
